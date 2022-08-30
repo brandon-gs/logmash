fe71f2b5780479a919d24aa7bab02c3bfec17b04
BrandonGS <brandongs180@gmail.com>
2022-08-30 13:31:58 -0500

    Adds Cypress to CI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styled from 'styled-components';
+import styled from "styled-components";
 import styles from "../styles/Home.module.css";
 
 const Container = styled.div`
